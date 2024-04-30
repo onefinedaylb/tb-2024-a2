73f78a104e148ba0696d2658ca5b9a617271bac6
onefinedaylb <psssjp22@example.com>
2024-04-30 12:18:29 +0900

    5단계 진행중

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,5 +1,7 @@
 package com.ll;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 class App {
@@ -7,6 +9,8 @@ class App {
         System.out.println("== 명언 앱 ==");
 
         int lastQuotationId = 0;
+
+        List<Quotation> quotations = new ArrayList<>();
 
         while(true){
             System.out.println("명령) ");
@@ -29,8 +33,12 @@ class App {
                 int id = lastQuotationId;
 
                 Quotation quotation = new Quotation(id, content, authorName);
+                quotations.add(quotation);
 
                 System.out.printf("%d번 명언이 등록되었습니다.\n", lastQuotationId);
+            }
+            else if(cmd.equals("목록")){
+                System.out.println("총 갯수 : " + quotations.size());
             }
         }
     }
