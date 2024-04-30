8a5c4f596f5e8d5f0688be4353309d68b7db590d
onefinedaylb <psssjp22@example.com>
2024-04-30 16:16:20 +0900

    getParamAsInt 함수 도입

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -36,7 +36,9 @@ class App {
             }
             else if(cmd.startsWith("삭제?")){
                 actionRemove(cmd);
-
+            }
+            else if (cmd.startsWith("수정?")){
+                actionModify(cmd);
             }
         }
     }
@@ -67,8 +69,26 @@ class App {
         }
     }
     void actionRemove(String cmd){
+        int id = getParamAsint(cmd, "id", 0);
+        if(id == 0) {
+            System.out.println("아이디를 정확히 입력해주세요.");
+            return; //함수를 끝낸다.
+        }
+
+        System.out.printf("%d번 명언을 삭제합니다.\n", id);
+    }
+    void actionModify(String cmd){
+        int id = getParamAsint(cmd, "id", 0);
+        if(id == 0) {
+            System.out.println("아이디를 정확히 입력해주세요.");
+            return; //함수를 끝낸다.
+        }
+
+        System.out.printf("%d번 명언을 수정합니다.\n", id);
+    }
+
+    int getParamAsint(String cmd, String paramName, int defaultValue){
         String[] cmdBits = cmd.split("\\?", 2);
-        String action = cmdBits[0];
         String queryString = cmdBits[1];
 
         String[] queryStringBits = queryString.split("&");
@@ -81,14 +101,19 @@ class App {
 
             String[] queryParamStrBits = queryParamStr.split("=", 2);
 
-            String paramName = queryParamStrBits[0];
+            String _paramName = queryParamStrBits[0];
             String paramValue = queryParamStrBits[1];
 
-            if (paramName.equals("id")) {
-                id = Integer.parseInt(paramValue);
+            if(_paramName.equals(paramName)){
+                try {
+                    //문제가 없을 경우
+                    return Integer.parseInt(paramValue);
+                } catch (NumberFormatException e) {
+                    //문제가 생긴 경우
+                    return defaultValue;
+                }
             }
         }
-
-        System.out.printf("%d번 명언을 삭제합니다.\n", id);
+        return defaultValue;
     }
 }
